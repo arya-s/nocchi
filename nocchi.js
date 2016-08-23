c803834554ec0cb4704e27023772a7cca529cce4
Andrei Borza <andrei.borza@gmail.com>
2016-08-24 01:45:00 +0900

    Add basic response feature, add cytube emotes

---
+++ nocchi.js
@@ -0,0 +1,44 @@
+var Discord = require('discord.js');
+var emotes  = require('./emotes');
+var nocchi  = new Discord.Client();
+var token   = process.env.DISCORD_TOKEN;
+var NAME    = 'nocchi';
+
+nocchi.loginWithToken(token, function output(error, token) {
+
+  if (error) {
+
+    console.log('Error', error);
+    return;
+
+  }
+
+});
+
+nocchi.on('message', function (data) {
+
+  var message = data.content.toLowerCase();
+  var user    = data.author.username;
+
+  // Don't react to ourselves
+  if (user === NAME) {
+    return;
+  }
+
+  if (message.indexOf('nocchi') > -1) {
+
+    data.channel.sendMessage('Hai, Nocchi desu.');
+
+  } else {
+
+    // Check emotes
+    var splitted = message.split(' ');
+    var emote    = splitted.find(function (e) { return e.indexOf('/' > -1); });
+
+    if (emotes.hasOwnProperty(emote)) {
+      data.channel.sendMessage(emotes[emote]);
+    }
+
+  }
+
+});
