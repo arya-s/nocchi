80c5002741e0814dec56eac2f82640d53144dc39
Adthrawn <adthrawn.ms@gmail.com>
2016-11-06 23:43:23 -0500

    Update add.js
    fix auto commit

--- src/commands/add.js
+++ src/commands/add.js
@@ -1,49 +1,68 @@
-import path from 'path';
-import jsonfile from 'jsonfile';
-import { getEmote, getImage } from '../util';
+'use strict';
 
-const emotesPath = path.join(__dirname, '../../assets/emotes.json');
-const assetsPath = path.join(__dirname, '../../assets');
-const emotes = require(emotesPath);
-const exec = require('child_process').exec;
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-class Command {
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-  constructor() {
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _jsonfile = require('jsonfile');
+
+var _jsonfile2 = _interopRequireDefault(_jsonfile);
+
+var _util = require('../util');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var emotesPath = _path2.default.join(__dirname, '../../assets/emotes.json');
+var assetsPath = _path2.default.join(__dirname, '../../assets');
+var emotes = require(emotesPath);
+var exec = require('child_process').exec;
+
+var Command = function () {
+  function Command() {
+    _classCallCheck(this, Command);
+
     this.aliases = ['add'];
   }
 
-  run(payload) {
+  _createClass(Command, [{
+    key: 'run',
+    value: function run(payload) {
+      var bot = payload.bot;
+      var message = payload.message;
+      var content = message.content;
 
-    const { bot, message } = payload;
-    const { content } = message;
-    const lower = content.toLowerCase();
+      var lower = content.toLowerCase();
 
-    if(lower.indexOf('add') === -1 || lower.indexOf('to') === -1 || !getEmote(content)) {
-      return bot.sendMessage(message, 'I couldn\'t add that emote');
-    }
-
-    addEmote(content, (error, response) => {
-
-      if (error) {
-
-        console.log('Error adding emote.', error);
-        return bot.sendMessage(message, 'Error adding emote. Please try again.');
-
+      if (lower.indexOf('add') === -1 || lower.indexOf('to') === -1 || !(0, _util.getEmote)(content)) {
+        return bot.sendMessage(message, 'I couldn\'t add that emote');
       }
 
-      bot.sendMessage(message, response);
+      addEmote(content, function (error, response) {
 
-    });
+        if (error) {
 
-  }
+          console.log('Error adding emote.', error);
+          return bot.sendMessage(message, 'Error adding emote. Please try again.');
+        }
 
-}
+        bot.sendMessage(message, response);
+      });
+    }
+  }]);
 
-const addEmote = function (message, done) {
+  return Command;
+}();
 
-  let emote = getEmote(message);
-  const image = getImage(message);
+var addEmote = function addEmote(message, done) {
+
+  var emote = (0, _util.getEmote)(message);
+  var image = (0, _util.getImage)(message);
 
   if (!image) {
     return done(null, 'That is not an image.');
@@ -51,57 +70,83 @@ const addEmote = function (message, done) {
 
   // If the emote already exist we want to find the appropriate counter to append to the emote handle
   if (emotes.hasOwnProperty(emote)) {
+    var _ret = function () {
 
-    // Find all emotes that match our emote name
-    let pattern = new RegExp(`${emote}(\\d+)`);
-    let keys = Object.keys(emotes);
-    let found = keys.reduce((prev, cur) => {
+      // Find all emotes that match our emote name
+      var pattern = new RegExp(emote + '(\\d+)');
+      var keys = Object.keys(emotes);
+      var found = keys.reduce(function (prev, cur) {
 
-      if (pattern.test(cur)) {
-        prev.push(cur);
+        if (pattern.test(cur)) {
+          prev.push(cur);
+        }
+
+        return prev;
+      }, [emote]).sort(function (first, second) {
+
+        // We need to sort by the ending numbers not by the entire string to catch /emote10 being placed before /emote2
+        return first.replace(emote, '') - second.replace(emote, '');
+      });
+
+      // Reject the emote if the image already exists
+      var _iteratorNormalCompletion = true;
+      var _didIteratorError = false;
+      var _iteratorError = undefined;
+
+      try {
+        for (var _iterator = found[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+          var e = _step.value;
+
+
+          if (emotes[e] === image) {
+            return {
+              v: done(null, 'That image already exists under ' + e)
+            };
+          }
+        }
+
+        // Find the appropriate counter
+      } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+          }
+        } finally {
+          if (_didIteratorError) {
+            throw _iteratorError;
+          }
+        }
       }
 
-      return prev;
+      var last = found[found.length - 1];
+      var counter = 2;
+      var matched = last.match(pattern);
 
-    }, [emote]).sort((first, second) => {
-
-      // We need to sort by the ending numbers not by the entire string to catch /emote10 being placed before /emote2
-      return first.replace(emote, '') - second.replace(emote, '');
-
-    });
-  
-    // Reject the emote if the image already exists
-    for (let e of found) {
-
-      if(emotes[e] === image){
-        return done(null, `That image already exists under ${e}`);
+      if (matched) {
+        counter = parseInt(matched[1], 10) + 1;
       }
 
-    }
+      emote = '' + emote + counter;
+    }();
 
-    // Find the appropriate counter
-    let last = found[found.length-1];
-    let counter = 2;
-    let matched = last.match(pattern);
-
-    if (matched) {
-      counter = parseInt(matched[1], 10) + 1;
-    }
-
-    emote = `${emote}${counter}`;
-
+    if ((typeof _ret === 'undefined' ? 'undefined' : _typeof(_ret)) === "object") return _ret.v;
   }
 
   emotes[emote] = image;
-  jsonfile.writeFile(emotesPath, emotes, { spaces: 2 }, error => {
+  _jsonfile2.default.writeFile(emotesPath, emotes, { spaces: 2 }, function (error) {
 
     if (error) {
       return console.log(error);
     }
 
-    done(error, `Added emote as ${emote}.`);
+    done(error, 'Added emote as ' + emote + '.');
     //change git push to the  <local branch>:<remote branch> for the branch you want to update
-	exec('git commit -m "emote update" ./assets/emotes.json;git push origin dev:dev')
+	if (process.env.NOCCHI_ENV === "production") {
+    exec('git commit -m "emote update" ./assets/emotes.json;git push origin master');
+	}
   });
 };
 
